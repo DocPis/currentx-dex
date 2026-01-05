7ee65d35156558cc5b0c62a098fb4a50f0095ac7
DocPis <docpis@tutamail.com>
2026-01-05 14:47:55 +0100

    a

--- src/shared/ui/Footer.jsx
+++ src/shared/ui/Footer.jsx
@@ -1,16 +1,16 @@
 import React from "react";
+import discordIcon from "../../assets/social/discord.png";
+import twitterIcon from "../../assets/social/x.png";
+import telegramIcon from "../../assets/social/telegram.png";
 
 const FOOTER_LINKS = {
   docs: "#",
-  twitter: "#",
-  discord: "#",
-  telegram: "#",
+  twitter: "https://x.com/currentxdex",
+  discord: "https://discord.gg/g33rC3RT",
+  telegram: "t.me/currentxdex",
 };
 
 export default function Footer() {
-  const iconClass =
-    "h-4 w-4 text-slate-300 group-hover:text-sky-300 transition-colors";
-
   const LinkItem = ({ href, children }) => (
     <a
       href={href}
@@ -46,43 +46,13 @@ export default function Footer() {
 
         <div className="flex items-center gap-2">
           <IconButton href={FOOTER_LINKS.discord} label="Discord">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              className={iconClass}
-            >
-              <path d="M8.5 9.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm8.5 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-              <path d="M16.25 17.25s-1 .75-4.25.75-4.25-.75-4.25-.75M9.5 7.5c0-.69.56-1.25 1.25-1.25h2.5c.69 0 1.25.56 1.25 1.25" />
-              <path d="M16 7.5s2 .5 3.5 1.5v6.5a2 2 0 0 1-2 2H6.5a2 2 0 0 1-2-2V9c1.5-1 3.5-1.5 3.5-1.5" />
-            </svg>
+            <img src={discordIcon} alt="Discord" className="h-4 w-4 object-contain" />
           </IconButton>
           <IconButton href={FOOTER_LINKS.twitter} label="Twitter / X">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              className={iconClass}
-            >
-              <path d="M5 5.5 18.5 19M5.5 19 19 5" />
-            </svg>
+            <img src={twitterIcon} alt="Twitter / X" className="h-4 w-4 object-contain" />
           </IconButton>
           <IconButton href={FOOTER_LINKS.telegram} label="Telegram">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              className={iconClass}
-            >
-              <path d="M19.5 4.5 4.75 10.75c-.89.37-.84 1.65.07 1.95L9.5 14.5l1.75 4.5c.32.83 1.5.87 1.89.05l6.36-13.66c.37-.8-.42-1.66-1.5-1.39Z" />
-              <path d="m9.5 14.5 2.5-2.5" />
-            </svg>
+            <img src={telegramIcon} alt="Telegram" className="h-4 w-4 object-contain" />
           </IconButton>
         </div>
       </div>
