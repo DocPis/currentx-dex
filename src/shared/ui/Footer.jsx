1225f4314d3a8bc101c7bd50ff4b02b203db61fc
DocPis <docpis@tutamail.com>
2026-01-05 16:04:28 +0100

    a

--- src/shared/ui/Footer.jsx
+++ src/shared/ui/Footer.jsx
@@ -4,7 +4,7 @@ import twitterIcon from "../../assets/social/x.png";
 import telegramIcon from "../../assets/social/telegram.png";
 
 const FOOTER_LINKS = {
-  docs: "https://currentx-dex.vercel.app/docs/intro",
+  docs: "https://currentx-docs.vercel.app/",
   twitter: "https://x.com/currentxdex",
   discord: "https://discord.gg/g33rC3RT",
   telegram: "https://t.co/VLEkH8Z2fD",
