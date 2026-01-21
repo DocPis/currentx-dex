82da705372cdcc4e10e0d0273f8d0feee5517c7f
DocPis <docpis@tutamail.com>
2026-01-21 01:02:14 +0100

    a

--- src/shared/config/networks.js
+++ src/shared/config/networks.js
@@ -40,7 +40,7 @@ const mainnetPreset = {
     "https://megaeth.blockscout.com",
   subgraphUrl: DEFAULT_MAINNET_SUBGRAPH_URL,
   subgraphApiKey: DEFAULT_MAINNET_SUBGRAPH_API_KEY,
-  rpcUrls: [env.VITE_RPC_URLS, env.VITE_RPC_URL, env.VITE_MEGAETH_RPC].filter(Boolean),
+  rpcUrls: [env.VITE_RPC_URL].filter(Boolean),
   addresses: {
     WETH_ADDRESS: "0x4200000000000000000000000000000000000006",
     USDC_ADDRESS: "0x4c99d545E82D32dA12Cc634a3964b1698073DA2B",
@@ -88,9 +88,7 @@ const testnetDefaults = {
 };
 
 const testnetPreset = (() => {
-  const rpcUrls = dedupeList([
-    env.VITE_TESTNET_RPC_URL || env.VITE_TESTNET_RPC_URLS || "",
-  ]);
+  const rpcUrls = dedupeList([env.VITE_TESTNET_RPC_URL, ...(testnetDefaults.rpcUrls || [])]);
   const addresses = {
     WETH_ADDRESS: env.VITE_TESTNET_WETH_ADDRESS || testnetDefaults.addresses.WETH_ADDRESS,
     USDC_ADDRESS: env.VITE_TESTNET_USDC_ADDRESS || testnetDefaults.addresses.USDC_ADDRESS,
